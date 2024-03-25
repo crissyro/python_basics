48c0e5a478e8c4f6968dd7b7783eb94d9307d436
Roman Moroz <morozdef24@gmail.com>
2024-03-25 17:30:51 +0300

    create insertion sort and decorator for testing

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,7 +1,24 @@
 import time
 
 
-def selection_sort(list:list):
+def time_Test(sort):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = sort(*args, **kwargs)
+        end_time = time.time()
+        print(f"lead time {sort.__name__}: {end_time - start_time} seconds")
+        return result
+    return wrapper
+
+
+#usage example
+
+#  @algorithms.sorting.time_Test
+#  def insertion_sorting_test(list):
+   #   return /../..insertion_Sort(list)
+
+
+def selection_Sort(list:list):
     n = len(list)
     for i in range(n-1):
         m = i
@@ -9,3 +26,17 @@ def selection_sort(list:list):
             if list[j] < list[m]:
                 m = j
         list[i], list[m] = list[m], list[i]
+
+    return list
+
+
+def insertion_Sort(list:list):
+    n = len(list)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if list[j] < list[j - 1]:
+                list[j], list[j - 1] = list[j - 1], list[j]
+            else:
+                break
+
+    return list
