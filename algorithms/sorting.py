a8a3e7ca5fe50a2f6e8daf0e20d1693e7ab2a0e7
Roman Moroz <morozdef24@gmail.com>
2024-04-03 19:51:06 +0300

    make heap sort

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -103,3 +103,33 @@ def quick_sort(lst: list) -> list:
         lst = quick_sort(low) + eq + quick_sort(hi)
 
     return lst
+
+
+# variant of heap sort
+def heapify(lst: list, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < n and lst[i] < lst[l]:
+        largest = l
+
+    if r < n and lst[largest] < lst[r]:
+        largest = r
+
+    if largest != i:
+        lst[i], lst[largest] = lst[largest], lst[i]
+
+        heapify(lst, n, largest)
+
+
+def heapSort(lst: list) -> list:
+    n = len(lst)
+
+    for i in range(n, -1, -1):
+        heapify(lst, n, i)
+
+    for i in range(n - 1, 0, -1):
+        lst[i], lst[0] = lst[0], lst[i]
+        heapify(lst, i, 0)
+
+    return lst
