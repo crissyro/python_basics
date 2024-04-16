84bd19ea0baf252be76a48ac91ec36ec1c6fb054
Roman Moroz <morozdef24@gmail.com>
2024-04-16 19:20:50 +0300

    create descriptor for example

--- oop_theory.py
+++ oop_theory.py
@@ -9,7 +9,7 @@ class Point:
     def __del__(self):
         print(f'Deleting {self}')
 
-#имитация запрета обращения к атрибуту x
+    #имитация запрета обращения к атрибуту x
     def __getattribute__(self, item):
         if item == 'x':
             raise AttributeError
@@ -17,7 +17,7 @@ class Point:
             print('Active magic magic method __getattribute__')
             return object.__getattribute__(self, item)
 
-#запрет на использования имени атрибута
+    #запрет на использования имени атрибута
     def __setattr__(self, key, value):
         if key == 'z':
             raise AttributeError
@@ -25,12 +25,12 @@ class Point:
             print('Active magic magic method __setattr__')
             object.__setattr__(self, key, value)
 
-# автоматически вызывается при обращение к несуществующему атрибуту
-# без него вылезает ошибка
+    # автоматически вызывается при обращение к несуществующему атрибуту
+    # без него вылезает ошибка
     def __getattr__(self, item):
         print(f"Active magic magic method __getattr__ + {item}")
 
-# удаляет локальное свойство с помощью ф-ции del
+    # удаляет локальное свойство с помощью ф-ции del
     def __delattr__(self, item):
         print(f"Active magic magic method __delattr__ + {item}")
         object.__delattr__(self, item)
@@ -60,6 +60,7 @@ class Database:
     def read(self):
         return "Data"
 
+
 # использовать декоратор @classmethod  для функций внутренних проверок,
 # иначе есть @staticmethod без скрытых аргументов
 
@@ -83,3 +84,36 @@ class ThreadData:
 
     def __init__(self):
         self.__dict__ = self.__shared_attrs
+
+
+# дескриптор
+
+class Integer:
+
+    @classmethod
+    def is_int(cls, value):
+        if type(value) != int:
+            raise TypeError
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        self.is_int(value)
+        setattr(instance, self.name, value)
+
+
+class Point:
+
+    x = Integer()
+    y = Integer()
+    z = Integer()
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
