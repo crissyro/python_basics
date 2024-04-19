53fc19b3d6ff291f6b2ac4045d983424e5250cfc
Roman Moroz <morozdef24@gmail.com>
2024-04-19 21:08:50 +0300

    change max value

--- coush_test.py
+++ coush_test.py
@@ -7,9 +7,9 @@ import math
 class Girl:
     girl_attr = {}
 
-    __MAX_HEIGHT = 1.95
-    __MAX_WEIGHT = 80
-    __MAX_AGE = 50
+    __MAX_HEIGHT = int(input('input your max height: '))
+    __MAX_WEIGHT = int(input('input your max weight: '))
+    __MAX_AGE = int(input('input your max age: '))
 
     def __init__(self, age: int, height: float, weight: float):
         self.girl_attr['age'] = age
@@ -17,22 +17,22 @@ class Girl:
         self.girl_attr['weight'] = weight
 
     @classmethod
-    def check_adecvatnost(self)->int:
-        res_check_adecvatnost = 0
+    def check_normal(self)->int:
+        res_check_normal = 0
 
         if self.girl_attr['height'] > self.__MAX_HEIGHT:
-            print(f"Рост {self.height} - ты пьяный?")
-            res_check_adecvatnost = -50
+            print(f' {self.girl_attr["height"]} is too big')
+            res_check_normal = -50
 
         if self.girl_attr['weight'] > self.__MAX_WEIGHT:
-            print(f"Вес {self.weight}-ты пьяный?")
-            res_check_adecvatnost = -50
+            print(f'{self.girl_attr["weight"]} is too big')
+            res_check_normal = -50
 
         if self.girl_attr['age'] > self.__MAX_AGE:
-            print(f"Возраст {self.age}-ты пьяный?")
-            res_check_adecvatnost = -50
+            print(f'{self.girl_attr["age"]} is too big')
+            res_check_normal = -50
 
-        return res_check_adecvatnost
+        return res_check_normal
 
     @classmethod
     def check_negative_attr(self):
