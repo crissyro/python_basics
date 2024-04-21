c314e71af53aaf508bc70236019373c38d6abdc8
Roman Moroz <morozdef24@gmail.com>
2024-04-21 20:33:29 +0300

    make directory tests/first use unittest

---
+++ tests/calculat_test.py
@@ -0,0 +1,40 @@
+from unittest import TestCase, main
+from simple_calculator import calculat
+
+
+class CalculatTest(TestCase):
+    def test_plus(self):
+        self.assertEqual(calculat('2+2'), 4)
+
+    def test_minus(self):
+        self.assertEqual(calculat('5-2'), 3)
+
+    def test_multiply(self):
+        self.assertEqual(calculat('5*2'), 10)
+
+    def test_divide(self):
+        self.assertEqual(calculat('10/5'), 2)
+
+    def test_no_signs(self):
+        with self.assertRaises(ValueError) as context:
+            calculat('10007')
+        self.assertEqual('Expression must have one signs', context.exception.args[0])
+
+    def test_no_signs(self):
+        with self.assertRaises(ValueError) as context:
+            calculat('2+2+2')
+        self.assertEqual('Expression must have two integer operands and one sign', context.exception.args[0])
+
+    def test_no_integers(self):
+        with self.assertRaises(ValueError) as context:
+            calculat('0.333 / 0.11')
+        self.assertEqual('Expression must have two integer operands and one sign', context.exception.args[0])
+
+    def test_strings(self):
+        with self.assertRaises(ValueError) as context:
+            calculat('q+w')
+        self.assertEqual('Expression must have two integer operands and one sign', context.exception.args[0])
+
+
+if __name__ == '__main__':
+    main()
