5ced87c55133e4e1c3c9e4893bd0fbda4ea52958
Roman Moroz <morozdef24@gmail.com>
2024-04-26 20:08:27 +0300

    remake idea

--- coush_test.py
+++ coush_test.py
@@ -1,12 +1,17 @@
 from accessify import private, protected
 
 
+class Person:
+    def __init__(self, age: int, height: int, cuteness: bool):
+        self.attributes = {'age': age, 'height': height, 'cuteness': cuteness}
 
-class Girl:
-    girl_attr = {}
 
-    def __init__(self, age: int, height: float, weight: float):
-        self.girl_attr['age'] = age
-        self.girl_attr['height'] = height
-        self.girl_attr['weight'] = weight
+
+
+
+class Man(Person):
+    def __init__(self, age: int, height: int, cuteness: bool):
+        super().__init__(self, age, height, cuteness)
+
+
 
